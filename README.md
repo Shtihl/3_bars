81bad28f7039c2ea3114e450951944922d99b6f9
Shtihl <shtihlfd@yandex.ru>
2017-11-21 18:07:54 +0300

    Initial Version

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+Скрипт для определения самого большого бара, самого маленького и самого ближайшего к заданным координатам. Поиск происходит на основе данных с [data.mos.ru](http://data.mos.ru).
+Для получения данных в формате JSON:
+
+1. Зарегистрироваться, получить ключ API
+2. Скачать файл по ссылке вида ```https://apidata.mos.ru/v1/features/1796?api_key={place_your_API_key_here}```
 
 # Как запустить
 
@@ -10,8 +14,19 @@
 
 ```bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+$ python bars.py path_to_json_file_with_data # possibly requires call of python3 executive instead of just python
+The biggest Bar: Спорт бар «Красная машина»
+Number of seats: 450
+
+The smallest Bar: Сушистор
+Number of seats: 0
+
+To find the nearest bar, enter your coordinates
+Enter your latitude 55.699058
+Enter your longitude 37.917861
+
+Nearest bar: Таверна
+Address: проспект Защитников Москвы, дом 8
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,21 +1,56 @@
+import sys
 import json
+from math import sqrt
 
 
-def load_data(filepath):
-    pass
+def load_json_data(source_file):
+    with open(source_file, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)['features']
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(parsed_json_data):
+    biggest_bar = max(parsed_json_data,
+                      key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+
+    return (biggest_bar['properties']['Attributes']['Name'],
+            biggest_bar['properties']['Attributes']['SeatsCount'])
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(parsed_json_data):
+    smallest_bar = min(parsed_json_data,
+                       key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+
+    return (smallest_bar['properties']['Attributes']['Name'],
+            smallest_bar['properties']['Attributes']['SeatsCount'])
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(parsed_json_data, longitude, latitude):
+    closest_bar = min(parsed_json_data,
+                      key=lambda point: get_distance(latitude,
+                                                     longitude,
+                                                     point['geometry']['coordinates']))
+    return (closest_bar['properties']['Attributes']['Name'],
+            closest_bar['properties']['Attributes']['Address'])
+
+
+def get_distance(latitude, longitude, user_point):
+    return sqrt((latitude - user_point[0])**2 + (longitude - user_point[1])**2)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+        print("The biggest Bar: {}\nNumber of seats: {}\n".format(
+            *get_biggest_bar(load_json_data(filepath))))
+        print("The smallest Bar: {}\nNumber of seats: {}\n".format(
+            *get_smallest_bar(load_json_data(filepath))))
+        print("To find the nearest bar, enter your coordinates")
+        user_latitude = float(input("Enter your latitude "))
+        user_longitude = float(input("Enter your longitude "))
+        print("\nNearest bar: {}\nAddress: {}".format(
+            *get_closest_bar(load_json_data(filepath),
+                             user_longitude,
+                             user_latitude)
+        ))
+    else:
+        print("Usage: python bars.py path_to_json_file")
