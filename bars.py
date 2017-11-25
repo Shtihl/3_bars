fcf8d152966cfaf77f92764cf5a91283406df95c
Dmitriy <shtihl.fd@gmail.com>
2017-11-25 03:31:19 +0300

    some logic fix

--- bars.py
+++ bars.py
@@ -11,39 +11,47 @@ def load_json_data(source_file):
 def get_biggest_bar_info(bars_info):
     biggest_bar = max(bars_info,
                       key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    bar_attributes = biggest_bar['properties']['Attributes']
-    return bar_attributes['Name'], bar_attributes['SeatsCount']
+    # bar_attributes = biggest_bar['properties']['Attributes']
+    return biggest_bar['properties']['Attributes']
+    # bar_attributes['Name'], bar_attributes['SeatsCount']
 
 
 def get_smallest_bar_info(bars_info):
     smallest_bar = min(bars_info,
                        key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    bar_attributes = smallest_bar['properties']['Attributes']
-    return bar_attributes['Name'], bar_attributes['SeatsCount']
+    # bar_attributes = smallest_bar['properties']['Attributes']
+    return smallest_bar['properties']['Attributes']
+    # bar_attributes['Name'], bar_attributes['SeatsCount']
 
 
-def get_closest_bar_info(bars_info, longitude, latitude):
+def get_closest_bar_info(bars_info, latitude, longitude):
     closest_bar = min(bars_info,
                       key=lambda point: sqrt(
                           (latitude - point['geometry']['coordinates'][0])**2 +
                           (longitude - point['geometry']['coordinates'][1])**2))
-    bar_attributes = closest_bar['properties']['Attributes']
-    return bar_attributes['Name'], bar_attributes['Address']
+    #bar_attributes = closest_bar['properties']['Attributes']
+    return closest_bar['properties']['Attributes']
+    # bar_attributes['Name'], bar_attributes['Address']
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         filepath = sys.argv[1]
         print("Самый большой бар: {}\nКоличество мест: {}\n".format(
-            *get_biggest_bar_info(load_json_data(filepath))))
+            get_biggest_bar_info(load_json_data(filepath))['Name'],
+            get_biggest_bar_info(load_json_data(filepath))['SeatsCount']))
         print("Самый маленький бар: {}\nКоличество мест: {}\n".format(
-            *get_smallest_bar_info(load_json_data(filepath))))
+            get_smallest_bar_info(load_json_data(filepath))['Name'],
+            get_smallest_bar_info(load_json_data(filepath))['SeatsCount']))
         print("Для определения ближайшего бара введите ваши координаты")
         user_latitude = float(input("Введите широту "))
         user_longitude = float(input("Введите долготу "))
         print("\nБлижайший бар: {}\nАдрес: {}".format(
-            *get_closest_bar_info(load_json_data(filepath),
-                             user_longitude,
-                             user_latitude)))
+            get_closest_bar_info(load_json_data(filepath),
+                                 user_longitude,
+                                 user_latitude)['Name'],
+            get_closest_bar_info(load_json_data(filepath),
+                                 user_longitude,
+                                 user_latitude)['Address']))
     else:
         print("Usage: python bars.py path_to_json_file")
