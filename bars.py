b735149676ea394af93421cdbb24ba6137819ebb
Shtihl <shtihlfd@yandex.ru>
2017-11-27 12:53:18 +0300

    Refactor bars.py

--- bars.py
+++ bars.py
@@ -9,29 +9,26 @@ def load_json_data(source_file):
 
 
 def get_biggest_bar_info(bars_info):
-    biggest_bar = max(bars_info,
-                      key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return biggest_bar['properties']
+    return max(bars_info,
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar_info(bars_info):
-    smallest_bar = min(bars_info,
-                       key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return smallest_bar['properties']
+    return min(bars_info,
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar_info(bars_info, latitude, longitude):
-    closest_bar = min(bars_info,
-                      key=lambda point: sqrt(
-                          (latitude - point['geometry']['coordinates'][0])**2 +
-                          (longitude - point['geometry']['coordinates'][1])**2))
-    return closest_bar['properties']
+    return min(bars_info,
+               key=lambda point: sqrt(
+                   (latitude - point['geometry']['coordinates'][0])**2 +
+                   (longitude - point['geometry']['coordinates'][1])**2))
 
 
 def print_bar_info(bar_info):
-    print("Название:", bar_info['Attributes']['Name'])
-    print("Количество мест:", bar_info['Attributes']['SeatsCount'])
-    print("Адрес:", bar_info['Attributes']['Address'])
+    print("Название:", bar_info['properties']['Attributes']['Name'])
+    print("Количество мест:", bar_info['properties']['Attributes']['SeatsCount'])
+    print("Адрес:", bar_info['properties']['Attributes']['Address'])
 
 
 if __name__ == '__main__':
